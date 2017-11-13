877bc874e3ef479ccbc3b1f378facf319a9e4c77
YosraBenAli <byhtuqgj1>
2017-11-13 10:05:26 +0100

    Displaying a List + Master/Detail Components + Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Hero } from '../hero';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +11,23 @@ import { Hero } from '../hero';
   encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
